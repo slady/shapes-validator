52352b3e08390f455d2f2cb9de7026dd32306145
Petr Sládek <petr.sladek@i.cz>
2017-05-20 14:43:07 +0200

    caching the color shape list map

--- src/main/java/cz/slady/shapes/ValidationModel.java
+++ src/main/java/cz/slady/shapes/ValidationModel.java
@@ -2,18 +2,21 @@ package cz.slady.shapes;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ValidationModel {
 
     private final List<Shape> shapeList;
+    private final Map<Color, List<Shape>> colorShapeListMap;
 
     public ValidationModel(final List<Shape> shapeList) {
         this.shapeList = shapeList;
+        this.colorShapeListMap = shapeList.stream().collect(Collectors.groupingBy(Shape::getColor));
     }
 
     public Collection<Shape> getShapesByColor(final Color color) {
-        return shapeList.stream().filter(shape -> shape.getColor() == color).collect(Collectors.toList());
+        return colorShapeListMap.get(color);
     }
 
     public List<Shape> getShapes() {
